9ce8023caac73eee7bf542c6e57a89e5bee4dde2
Matt Balmer <signup@mattbalmer.com>
2024-10-02 14:56:42 +0200

    Integrate edge rendering & a bit of node rewriting

--- client/source/@game/components/Game.tsx
+++ client/source/@game/components/Game.tsx
@@ -6,6 +6,7 @@ import { Box } from '@mui/material';
 
 type NodeID = `${string}`;
 type EdgeString = `${string}:${string}`;
+type Dir = 'up' | 'down' | 'right' | 'left';
 
 type Node = {
   x: number,
@@ -19,6 +20,33 @@ type Game = {
 }
 
 const coordToString = ({ x, y }) => `${x},${y}`;
+
+const getEdgeDirs = (nodeMap: Record<string, string>, { x, y }: { x: number, y: number }): Dir[] => {
+  let output: Dir[] = [];
+
+  if (coordToString({ x: x - 0.5, y }) in nodeMap && coordToString({ x: x + 0.5, y }) in nodeMap) {
+    output.push('left');
+    output.push('right');
+  }
+  if (coordToString({ x, y: y - 0.5 }) in nodeMap && coordToString({ x, y: y + 0.5 }) in nodeMap) {
+    output.push('up');
+    output.push('down');
+  }
+  // if (coordToString({ x: x - 0.5, y }) in nodeMap) {
+  //   output.push('left');
+  // }
+  // if (coordToString({ x: x + 0.5, y }) in nodeMap) {
+  //   output.push('right');
+  // }
+  // if (coordToString({ x, y: y - 0.5 }) in nodeMap) {
+  //   output.push('up');
+  // }
+  // if (coordToString({ x, y: y + 0.5 }) in nodeMap) {
+  //   output.push('down');
+  // }
+
+  return output
+}
 
 const useGame = () => {
   const [game, setGame] = useState<Game>({
@@ -45,6 +73,55 @@ const useGame = () => {
   }
 }
 
+const Node = ({ id, selected, exist }: {
+  id: string,
+  selected: boolean,
+  exist?: boolean,
+}) => {
+  return <Box
+    data-key={id}
+    sx={{
+      width: 50,
+      height: 50,
+      background: selected ? 'red' : exist === true ? 'blue' : '#222',
+      border: '1px solid white',
+      borderRadius: '50%',
+    }}
+  />
+}
+
+const Edge = ({ id, dirs }: {
+  id: string,
+  dirs: Dir[],
+}) => {
+  return <Box
+    data-key={id}
+    sx={{
+      position: 'relative',
+      width: 50,
+      height: 50,
+    }}
+  >
+    {dirs.filter(dir => dir === 'left' || dir === 'up').map(dir => {
+      return <Box
+        key={dir}
+        data-key={dir}
+        sx={{
+          width: dir === 'left' ? 30 : 10,
+          height: dir === 'up' ? 30 : 10,
+          background: 'green',
+          border: '1px solid white',
+          borderRadius: 10,
+          position: 'absolute',
+          top: dir === 'up' ? 10 : '50%',
+          left: dir === 'left' ? 10 : '50%',
+          transform: dir === 'up' ? 'translateX(-50%)' : 'translateY(-50%)',
+        }}
+      />
+    })}
+  </Box>
+}
+
 export const Game = () => {
   const { game, setGame } = useGame();
 
@@ -56,10 +133,10 @@ export const Game = () => {
         ...m,
         [coord]: keys[i],
       }
-    }, {});
+    }, {} as Record<string, NodeID>);
   }, [game.nodes, game.hovered]);
 
-  const handleKeyDown = (e) => {
+  const handleKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'ArrowUp') {
       setGame((prev) => {
         const hovered = prev.hovered;
@@ -122,6 +199,7 @@ export const Game = () => {
     }
   }
 
+  // Assign key down
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
 
@@ -130,21 +208,36 @@ export const Game = () => {
     }
   }, []);
 
+  const size = [-2, -1, 0, 1, 2].reduce((s, n) => [...s, n, n + 0.5], []).slice(0, -1);
+
   return <div>
     <FlexCol
       sx={{ flexGrow: 1, height: '100vh', background: '#111', alignItems: 'center', justifyContent: 'center' }}
     >
       <FlexRow sx={{ alignItems: 'center' }}>
-        {[0,1,2,3,4,5].map(x => {
-          return <FlexCol key={`${x}`} sx={{ alignItems: 'center' }}>
-            {[0,1,2,3,4,5].map(y => {
+        {size.slice(0).map(x => {
+          return <FlexCol
+            key={`${x}`}
+            sx={{ alignItems: 'center' }}
+          >
+            {size.slice(0).map(y => {
               const coordStr = coordToString({ x, y });
-              return <Box data-key={coordStr} key={coordStr} sx={{
-                width: 50,
-                height: 50,
-                background: coordStr === hoveredNode ? 'red' : coordStr in nodeMap ? 'blue' : '#222',
-                border: '1px solid white' }}
-              />
+              if (x % 1 === 0 && y % 1 === 0) {
+                return <Node
+                  id={coordStr}
+                  key={coordStr}
+                  selected={coordStr === hoveredNode}
+                  exist={coordStr in nodeMap}
+                />
+              } else {
+                const dirs = getEdgeDirs(nodeMap, { x, y });
+                console.log('dirs', x, y, dirs);
+                return <Edge
+                  id={coordStr}
+                  key={coordStr}
+                  dirs={dirs}
+                />
+              }
             })}
           </FlexCol>
         })}
